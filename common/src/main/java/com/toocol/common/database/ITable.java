13594be11231b452655a7da69e4a530778d0bdb7
Joezeo <1252795700@qq.com>
2021-05-03 17:59:01 +0800

    code push => 1.代码结构修改

---
+++ common/src/main/java/com/toocol/common/database/ITable.java
@@ -0,0 +1,13 @@
+package com.toocol.common.database;
+
+import java.io.Serializable;
+
+/**
+ * this is the annotation for mysql pojo
+ *
+ * @author Joezeo
+ * @date 2021/5/3 17:54
+ */
+public interface ITable extends Serializable {
+
+}
